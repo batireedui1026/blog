675a38f2e96fbc6681f6610724faf692c21b54a1
batireedui <batireeduiotgonsukh@gmail.com>
2024-08-13 08:51:20 +0800

    yuch nemeegui bolno

--- src/provider/search-provider.jsx
+++ src/provider/search-provider.jsx
@@ -1,18 +1,4 @@
-// import { createContext, useState } from "react";
 
-// export const SearchContext = createContext(null);
-
-// const SearchProvider = ({ children }) => {
-// 	const [searchValue, setSearchValue] = useState("");
-
-// 	return (
-// 		<SearchContext.Provider value={{ searchValue, setSearchValue }}>
-// 			{children}
-// 		</SearchContext.Provider>
-// 	);
-// };
-
-// export default SearchProvider;
 import { createContext, useState } from "react";
 export const SearchContext = createContext(null);
 
